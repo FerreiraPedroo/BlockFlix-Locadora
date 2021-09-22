232b1524501f39be70a6c804aeafe4c696eaafd2
Simplerino <73146680+FerreiraPedroo@users.noreply.github.com>
2021-09-22 17:42:26 -0300

    Inicio

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BlockFlix from './BlockFlix';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BlockFlix />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
