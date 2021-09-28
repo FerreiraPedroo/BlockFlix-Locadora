c879a1c70c9211047a8ef3ac40390daf720fd521
Simplerino <73146680+FerreiraPedroo@users.noreply.github.com>
2021-09-28 11:02:11 -0300

    Atualização #5.1 - fixando a referencia no index do blockflix.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import BlockFlix from 'blockflix.js';
+import BlockFlix from './blockflix.js';
 
 
 ReactDOM.render(
